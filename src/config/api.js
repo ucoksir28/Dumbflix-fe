b531c7b6d4a1553505e95f5e08370280640a2ef5
ucoksir28 <96972678+ucoksir28@users.noreply.github.com>
2022-03-21 06:12:36 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // Set config defaults when creating the instance
 export const API = axios.create({
-	baseURL: 'http://localhost:5000/api/v1'
+	baseURL: 'http://api.teknoinet.com/api/v1'
 });
 
 // Alter defaults after instance has been created
